b84b6f52c6d611350d08a71ca84977bc711ad646
bora-sen <bahattinborasen@gmail.com>
2024-11-19 15:05:00 +0300

    Update CardPreview component

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -4,15 +4,15 @@ import s from "./CardPreview.module.css"
 function CardPreview() {
   const { state } = useConfigContext()
   return (
-    <div className="container d-flex align-items-center justify-content-center p-5">
+    <div className="container d-flex align-items-center justify-content-center p-5 fw-bold">
       <div id="eqsl_card_preview" style={{ backgroundImage: `url(${state.bgUrl})` }} className={`preview-container shadow position-relative pe-none`}>
         <span className={`${s.previewCallsign} text-${state.callsignPos}`}>{state.callsign}</span>
         <span className={`${s.previewNameSurname} text-${state.nameSurnamePos}`}>{state.nameSurname}</span>
         <div className={`${s.previewInfoContainer} text-${state.qsoInfoPos}`}>
           <span>
-            TO: {state.toCallSign} Confirming 2-Way SSB QSO, Band: {state.band}
+            TO: {state.toCallSign} Confirming 2-Way {state.mode} QSO, Band: {state.band}
           </span>
-          <span>Date: {state.date}</span>
+          <span>Date: {state.date.toString()}</span>
         </div>
         <div>
           <span className={`${s.previewAddress} text-${state.stateCountryPos}`}>
